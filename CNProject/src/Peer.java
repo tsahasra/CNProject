7fd9e19be94740552673aa9d832effaa44dae4ee
Tejas <Tejas@Tejas-PC>
2017-02-14 18:38:44 -0500

    Updated peerProcess.java

--- CNProject/src/Peer.java
+++ CNProject/src/Peer.java
@@ -7,7 +7,15 @@
  *
  */
 public class Peer {
-
+	
+	String peerID;
 	String peerIP;
 	String peerPort;
+	
+	public Peer(String pid, String pip, String pport)
+	{
+		this.peerID = pid;
+		this.peerIP = pip;
+		this.peerPort = pport;
+	}
 }
