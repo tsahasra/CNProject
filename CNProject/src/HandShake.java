2580d7f2a7d7d7aabb3929f76b403bc72a92dcc9
Apurv Amrutkar <Apurv Amrutkar>
2017-04-07 11:12:04 -0400

    serialVersionId

--- CNProject/src/HandShake.java
+++ CNProject/src/HandShake.java
@@ -1,7 +1,15 @@
+import java.io.DataOutputStream;
+import java.io.IOException;
 import java.io.Serializable;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 
 public class HandShake implements Serializable{
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	String header ;
 	String zerobits;
 	int peerID;
@@ -11,5 +19,14 @@ public class HandShake implements Serializable{
 		peerID = peerId;
 		zerobits = "0000000000";
 	} 
+	
+	public void write(DataOutputStream outputStream) throws IOException {
+        byte[] protocolId = header.getBytes();
+        byte[] zero = zerobits.getBytes();
+        byte[] peerIdBits = ByteBuffer.allocate(4).order(ByteOrder.BIG_ENDIAN).putInt(peerID).array();
+        outputStream.write (protocolId, 0, protocolId.length);
+        outputStream.write(zero, 0, zero.length);
+        outputStream.write(peerIdBits, 0, peerIdBits.length);
+    }
 		
 }
