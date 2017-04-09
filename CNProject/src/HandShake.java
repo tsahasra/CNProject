2e66c9cd8b87deb105210ad82531ebd862cadd40
Apurv Amrutkar <Apurv Amrutkar>
2017-04-09 13:54:56 -0400

    header to 18 byte char array HandShake.java

--- CNProject/src/HandShake.java
+++ CNProject/src/HandShake.java
@@ -7,12 +7,17 @@ public class HandShake extends Message implements Serializable{
 	 * 
 	 */
 	private static final long serialVersionUID = 8799977982265952720L;
-	String header ;
+	byte[] header;
 	byte[] zerobits;
 	int peerID;
 	
 	public HandShake(int peerId){
-		header = "P2PFILESHARINGPROJ";
+		String h = "P2PFILESHARINGPROJ";
+		
+		header = new byte[18];
+		for(int i=0;i<h.length();i++){
+			header[i]=(byte)(h.charAt(i));
+		}
 		peerID = peerId;
 		zerobits = new byte[10];
 		for(int i =0 ; i<10 ; i++)
